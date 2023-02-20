4228138b7ab0331afb94ac76f3c19848ca4b0322
37Reese <reesedugg24@gmail.com>
2023-02-19 21:42:46 -0900

    list Revisist

---
+++ Inspire-Youth-In-STEM/Week-3/List_revisited.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# This is a single line comment
+# Python program to illustrate operators
+# Name : Cyril Newman
+# Email : reesedugg24@gmail.com
+# Date : 20th Feb 2023
+# File : List_revisited.py
+'''
+#camel case
+myFavouriteFruits = ["peaches","banana","avocado","mango","raspberry"]
+for fruit in myFavouriteFruits :
+    print(fruit)
+
+print(len(myFavouriteFruits))
+'''
+myFriends = ["Neema","Voke","Cecil","Rehema","Reese","Prince","Dugg"]
+print(myFriends)
+myFriends[3] = "Newton"
+print(myFriends)
+print("---------------------------------------------------------------")
+new_friends = myFriends.copy()
+print(new_friends)
+
+new_friends.sort()
+print(new_friends)
+
+
---
+++ Inspire-Youth-In-STEM/Week-3/assignment.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# This is a single line comment
+# Create an empty list of favourite musicians
+# Using for loop add 5 new musicians
+# copy the list ta new list called celebs\
+# sort the list
+# pop out two celebs from the list
+# Count the remaining celebs in the list
+# Name : Cyril Newman
+# Email : reesedugg24@gmail.com
+# Date : 20th Feb 2023
+# File : assignment.py
